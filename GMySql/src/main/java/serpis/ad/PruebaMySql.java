3e4bacb11ea8fe878afe0fc65933718eb6b48c49
oscarcillo18 <oscar.gimenez1997@gmail.com>
2017-11-27 13:48:16 +0100

    Nuevo GMySql

--- GMySql/src/main/java/serpis/ad/PruebaMySql.java
+++ GMySql/src/main/java/serpis/ad/PruebaMySql.java
@@ -2,15 +2,40 @@ package serpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 public class PruebaMySql {
 
-	public static void main(String[] args) throws SQLException {
-		// TODO conectar
+	public static void main(String[] args) throws SQLException , ClassNotFoundException {
+		String DB_URL = "jdbc:mysql://localhost/dbprueba";
+		String USER = "root";
+		String PASS = "sistemas";
+		String sql; 
+		
 		Connection connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		
+		Statement stmt = connection.createStatement();
 	
-	connection.close();
+		
+		sql = "SELECT * FROM categoria";
+	    ResultSet rs = stmt.executeQuery(sql);
+	    
+	    while(rs.next()){
+	         int id  = rs.getInt("id");
+	         String nombre = rs.getString("nombre");
+
+	         System.out.println("ID: " + id + " nombre: " + nombre);
+	      }
+	    
+	      rs.close();
+	      stmt.close();
+	      connection.close();
 	}
-}
+
+	}
+
